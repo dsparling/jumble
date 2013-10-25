f81f8762c9c88a210acf2fc88c4dc438b6ad818c
Doug Sparling <dsparling@amuniversal.com>
2013-10-25 17:17:06 -0500

    updated specs

--- spec/jumble_spec.rb
+++ spec/jumble_spec.rb
@@ -6,23 +6,24 @@ describe "Jumble" do
     @j = Jumble.new()
   end
 
-  it "solve word for solvable word" do
+  it "returns words for solvable word" do
     good_words = @j.solve_word('rta')
     expect(good_words).to_not be_empty
   end
 
-  it "not solve word for non-solvable word" do
+  it "does not return words for non-solvable word" do
     good_words = @j.solve_word('rtartartarta')
     expect(good_words).to be_empty
   end
 
-  it "jumble word" do
+  it "jumbles a word" do
     word = 'ruby'
     jumbled_word = @j.jumble_word(word)
+    expect(jumbled_word).not_to be_empty
     expect(jumbled_word).not_to eq(word)
   end
 
-  it "create jumble" do
+  it "creates a jumble" do
     @j.word_lengths_allowed(3,4)
     jumble = @j.create_jumble
     expect(jumble.length).to eq(@j.num_words)
