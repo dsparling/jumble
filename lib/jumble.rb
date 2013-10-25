e6a2d44ed9acdf3d2f6974ec3758cf7db3df8632
Doug Sparling <dsparling@amuniversal.com>
2013-10-25 12:14:44 -0500

    removed old newgem code

--- lib/jumble.rb
+++ lib/jumble.rb
@@ -1,14 +1,13 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-class Jumble 
-  VERSION = '0.2.0'
+class Jumble
 
   attr_accessor :dict, :num_words
 
   def initialize(dict='/usr/share/dict/words')
-    @dict                      = dict 
-    @num_words                 = 5 
+    @dict                      = dict
+    @num_words                 = 5
     @_word_lengths_allowed     = {}
     @_word_lengths_not_allowed = {}
   end
@@ -27,7 +26,7 @@ class Jumble
         line.chomp!.downcase!
         next if line !~ /^[a-z]+$/          # Letters only
         next if line.length != word.length # Same length as key
-  
+
         # Create canonical version of the dict word
         line_key = line.split("").sort.join
 
@@ -52,15 +51,15 @@ class Jumble
 
 
   def create_jumble
-    
+
     words = {}
-    
+
     # Read dictionary and get words same length as word 
     File.open(@dict, 'r') do |file|
       while (line = file.gets)
         line.chomp!.downcase!
         next if line !~ /^[a-z]+$/          # Letters only
-  
+
         # Create canonical version of the dict word
         line_key = line.split("").sort.join
 
@@ -114,7 +113,7 @@ class Jumble
       jumble_words << unique_words[rand(unique_words.size)]
     end
 
-    # Scramble the words 
+    # Scramble the words
     jumbled_words = []
     jumble_words.each do |w|
       jumbled_words << self.jumble_word(w[0]) 
